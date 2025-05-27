5b904e4622bd856c462d9fce45ea9bb8721243cf
Vangnhya Matavele <vangnhya.matavele@gmail.com>
2025-05-27 16:10:48 +0200

    feat: enhance game interface with monster stats and introductory text

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterName = document.querySelector("#monsterName");
+const monsterStats = document.querySelector("#monsterStats");
+monsterHealth= document.querySelector("#monsterHealth");
+const mHealth = monsterHealth;
+
+function goStore() {
+  console.log("Going to store.");
+}
+
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");;
+}
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
